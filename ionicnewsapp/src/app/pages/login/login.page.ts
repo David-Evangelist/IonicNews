e73dd96c2ffcde62d9866a427733f9c721d7a7b7
Kathyllin <kathyllinbandeira83@gmail.com>
2025-05-31 17:59:05 -0300

    revisamentodoCss

--- ionicnewsapp/src/app/pages/login/login.page.ts
+++ ionicnewsapp/src/app/pages/login/login.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router'; 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: false,
 })
-export class LoginPage implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
-
+export class LoginPage {
   email: string = '';
   senha: string = '';
+ nome: string = '';
+  constructor(private router: Router) {}
 
   login() {
-    console.log('Login com:', this.email, this.senha);
+    if (this.nome && this.email && this.senha) {
+   
+      localStorage.setItem('usuarioNome', this.nome);
+      localStorage.setItem('usuarioEmail', this.email);
+
+      this.router.navigateByUrl('/news');
+    } else {
+      alert('Por favor, preencha todos os campos.');
+    }
   }
 }
+
--- ionicnewsapp/src/app/pages/news/news.page.ts
+++ ionicnewsapp/src/app/pages/news/news.page.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  NewsService,
-  NewsApiResponse,
-  Article,
-} from '../../services/news.service';
+import { Router } from '@angular/router';
+import { NewsService } from '../../services/news.service';
 
 @Component({
   selector: 'app-news',
@@ -12,30 +9,59 @@ import {
   standalone: false,
 })
 export class NewsPage implements OnInit {
-  constructor(private newsService: NewsService) {}
+  usuario: string = 'Visitante';
+
+  manchete: { titulo: string; imagem: string; link: string } | null = null;
+  noticias: Array<{ titulo: string; imagem: string; link: string }> = [];
+
+  constructor(private newsService: NewsService, private router: Router) {}
 
   ngOnInit() {
+    this.carregarUsuario();
     this.loadNews();
   }
 
-  noticias: Array<{ titulo: string; imagem: string; link: string }> = [];
+  carregarUsuario(): void {
+    const usuarioArmazenado = localStorage.getItem('usuarioNome'); 
+    if (!usuarioArmazenado) {
+      this.router.navigateByUrl('/login');
+    } else {
+      this.usuario = usuarioArmazenado;
+    }
+  }
 
   loadNews(): void {
     this.newsService.getAppleNews().subscribe({
-      next: (data: NewsApiResponse) => {
-        this.noticias = data.articles.map((article: Article) => ({
+      next: (data) => {
+        const artigos = data.articles.map(article => ({
           titulo: article.title,
           imagem: article.urlToImage ?? '',
           link: article.url,
         }));
+
+        if (artigos.length > 0) {
+          this.manchete = artigos[0];
+          this.noticias = artigos.slice(1);
+        } else {
+          this.manchete = null;
+          this.noticias = [];
+        }
       },
       error: (err) => {
         console.error('Erro ao carregar notícias:', err);
+        this.manchete = null;
+        this.noticias = [];
       },
     });
   }
 
   abrirLink(url: string): void {
     window.open(url, '_blank');
+  }
+
+  logout(): void {
+    localStorage.removeItem('usuarioNome'); 
+    localStorage.removeItem('usuarioEmail'); 
+    this.router.navigateByUrl('/login');
   }
 }
